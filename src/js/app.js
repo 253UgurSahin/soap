5b1ff34246f54232fd07b83c03ae51eb056f4959
253ugursahin <ugurs156@gmail.com>
2022-08-10 23:19:13 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,57 @@
+import readXlsxFile from 'read-excel-file'
+
+function soap() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'https://cors-anywhere.herokuapp.com/http://ec.europa.eu/taxation_customs/vies/services/checkVatService', true);
+
+    // build SOAP request
+    var sr =`
+        <SOAP-ENV:Envelope xmlns:ns0='urn:ec.europa.eu:taxud:vies:services:checkVat:types' xmlns:ns1='http://schemas.xmlsoap.org/soap/envelope/' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:SOAP-ENV='http://schemas.xmlsoap.org/soap/envelope/'>
+        <SOAP-ENV:Header/>
+            <ns1:Body>
+                <ns0:checkVat>
+                    <ns0:countryCode>FR</ns0:countryCode>
+                    <ns0:vatNumber>23</ns0:vatNumber>
+                </ns0:checkVat>
+            </ns1:Body>
+        </SOAP-ENV:Envelope>
+    `;
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                let xmlDoc = xmlhttp.responseXML;
+                let x = xmlDoc.getElementsByTagName("valid")[0];
+                // let y = x.childNodes[0];
+                // let z = y.nodeValue;
+                console.log(x.textContent);
+            }
+        }
+    }
+    // Send the POST request
+    xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+    xmlhttp.send(sr);
+}
+
+
+function readXls() {    
+    const input = document.getElementById('input');
+
+    let arr = [];
+
+    input.addEventListener('change', () => {
+        readXlsxFile(input.files[0]).then((rows) => {
+            console.log(rows.length);
+
+            rows.forEach((row, i) => {
+                arr += row
+            });
+        })
+    });
+
+    console.table(arr);
+}
+
+
+readXls();
+
